c4e28e1bb48c89bf3e0e765acaa340efbab3d7f9
Ermal1 <ermal_aliu10@hotmail.com>
2024-07-14 08:48:44 +0200

    Initial commit

---
+++ CountingPathsTask/Program.cs
@@ -0,0 +1,66 @@
+﻿class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.Write("Enter the value of X: ");
+            int X = int.Parse(Console.ReadLine());
+            Console.Write("Enter the value of Y: ");
+            int Y = int.Parse(Console.ReadLine());
+
+            List<string> paths = new List<string>();
+
+            if (X == 0 && Y == 0)
+            {
+                Console.WriteLine("1");
+            }
+            else
+            {
+                if (X == 0)
+                {
+                    paths.Add(new string('N', Y));
+                }
+                else if (Y == 0)
+                {
+                    paths.Add(new string('E', X));
+                }
+                else
+                {
+                    CountingPaths(X, Y, "", 0, 0, ' ', paths);
+                }
+
+                Console.WriteLine($"Number of valid paths: {paths.Count}");
+                Console.Write("Routes for each valid path: ");
+                Console.WriteLine(string.Join(",", paths));
+            }
+
+            Console.Write("Would you like one more try? (yes/no): ");
+            string answer = Console.ReadLine().Trim().ToLower();
+
+            if (answer != "yes")
+            {
+                break;
+            }
+        }
+    }
+
+    static void CountingPaths(int X, int Y, string path, int xNorth, int yEast, char lastMove, List<string> paths)
+    {
+        if (xNorth == X && yEast == Y)
+        {
+            paths.Add(path);
+            return;
+        }
+
+        if (xNorth < X && !(lastMove == 'E' && path.EndsWith("EE")))
+        {
+            CountingPaths(X, Y, path + 'E', xNorth + 1, yEast, 'E', paths);
+        }
+
+        if (yEast < Y && !(lastMove == 'N' && path.EndsWith("NN")))
+        {
+            CountingPaths(X, Y, path + 'N', xNorth, yEast + 1, 'N', paths);
+        }
+    }
+}
